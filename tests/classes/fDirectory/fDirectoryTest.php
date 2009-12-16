35c6ebd61e8f4d073ce2f19183ecac35d3e2c7db
Will Bond <will@flourishlib.com>
2009-12-16 07:03:54 +0000

    BackwardsCompatibilityBreak - Renamed fFile::getFilename() to fFile::getName(), fFile::getFilesize() to fFile::getSize(), fFile::getDirectory() to fFile::getParent(), fDirectory::getFilesize() to fDirectory::getSize(). Added fDirectory::getName(), fDirectory::move() and fFile::move().
    git-svn-id: http://svn.flourishlib.com/trunk@736 8e09ec71-114c-0410-a285-86ecef5c4c40

---
+++ tests/classes/fDirectory/fDirectoryTest.php
@@ -0,0 +1,109 @@
+<?php
+require_once('./support/init.php');
+ 
+class fDirectoryTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+		mkdir('output/fDirectory/');
+		file_put_contents('output/fDirectory/test.txt', 'test');
+				
+	}
+	
+	public function testCreate()
+	{
+		$dir = fDirectory::create('output/fDirectory2/');
+		$this->assertEquals(TRUE, $dir instanceof fDirectory);
+	}
+	
+	public function testConstruct()
+	{
+		$dir = new fDirectory('output/fDirectory/');
+		$this->assertEquals(TRUE, $dir instanceof fDirectory);
+	}
+	
+	public function testConstructNoDir()
+	{
+		$this->setExpectedException('fValidationException');
+		$file = new fFile('output/fDirectory2/');
+	}
+	
+	public function testConstructRegFile()
+	{
+		$this->setExpectedException('fValidationException');
+		$dir = new fDirectory('output/fDirectory/test.txt');
+	}
+	
+	public function testToString()
+	{
+		$dir = new fDirectory('output/fDirectory/');
+		$this->assertEquals($_SERVER['DOCUMENT_ROOT'] . '/output/fDirectory/', $dir->__toString());
+	}
+	
+	public function testGetName()
+	{
+		$dir = new fDirectory('output/fDirectory/');
+		$this->assertEquals('fDirectory', $dir->getName());
+	}
+	
+	public function testGetParent()
+	{
+		$dir = new fDirectory('output/fDirectory/');
+		$this->assertEquals(TRUE, $dir->getParent() instanceof fDirectory);
+		$this->assertEquals(dirname($dir->getPath()) . DIRECTORY_SEPARATOR, $dir->getParent()->getPath());
+	}
+	
+	public function testGetPath()
+	{
+		$dir = new fDirectory('output/fDirectory/');
+		$this->assertEquals('/output/fDirectory/', str_replace($_SERVER['DOCUMENT_ROOT'], '', $dir->getPath()));
+	}
+	
+	public function testGetSize()
+	{
+		$dir = new fDirectory('output/fDirectory/');
+		$this->assertEquals(4, $dir->getSize());
+	}
+	
+	public function testRename()
+	{
+		$dir = new fDirectory('output/fDirectory/');
+		$dir->rename('fDirectory3', TRUE);
+		$this->assertEquals('fDirectory3', $dir->getName());
+		$this->assertEquals($_SERVER['DOCUMENT_ROOT'] . '/output/', $dir->getParent()->getPath());
+	}
+	
+	public function testRenameAnotherDirectory()
+	{
+		$dir = fDirectory::create('output/fDirectory2/');
+		$dir->rename('output/fDirectory/fDirectory3', TRUE);
+		$this->assertEquals('fDirectory3', $dir->getName());
+		$this->assertEquals($_SERVER['DOCUMENT_ROOT'] . '/output/fDirectory/', $dir->getParent()->getPath());
+	}
+	
+	public function testMove()
+	{
+		$dir = fDirectory::create('output/fDirectory2/');
+		$dir->move('output/fDirectory/', TRUE);
+		$this->assertEquals('fDirectory2', $dir->getName());
+		$this->assertEquals($_SERVER['DOCUMENT_ROOT'] . '/output/fDirectory/', $dir->getParent()->getPath());
+	}
+	
+	public function tearDown()
+	{
+		$dirs = array('output/fDirectory/', 'output/fDirectory2/', 'output/fDirectory3/');
+		foreach ($dirs as $dir) {
+			if (file_exists($dir)) {
+				$files = array_diff(scandir($dir), array('.', '..'));
+				foreach ($files as $file) {
+					if (is_dir($dir . $file)) {
+						rmdir($dir . $file);	
+					} else {
+						unlink($dir . $file);
+					}
+				}
+				rmdir($dir);
+			}
+		}
+	}
+}
---
+++ tests/classes/fFile/fFileTest.php
@@ -0,0 +1,115 @@
+<?php
+require_once('./support/init.php');
+ 
+class fFileTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{	
+		mkdir('output/fFile/');
+		mkdir('output/fFile2/');
+		file_put_contents('output/fFile/one.txt', 'one');
+		file_put_contents('output/fFile/two.txt', 'two');	
+	}
+	
+	public function testCreate()
+	{
+		$file = fFile::create('output/fFile/three.txt', 'thr33');
+		$this->assertEquals(TRUE, $file instanceof fFile);
+		$this->assertEquals('thr33', file_get_contents('output/fFile/three.txt'));	
+	}
+	
+	public function testClone()
+	{
+		$file     = fFile::create('output/fFile/three.txt', 'thr33');
+		$new_file = clone $file;
+		$this->assertEquals(TRUE, $new_file instanceof fFile);
+		$this->assertEquals(TRUE, $new_file->getPath() != $file->getPath());
+		$this->assertEquals('thr33', file_get_contents($new_file->getPath()));	
+	}
+	
+	public function testConstruct()
+	{
+		$file     = new fFile('output/fFile/one.txt');
+		$this->assertEquals(TRUE, $file instanceof fFile);
+		$this->assertEquals('one', file_get_contents($file->getPath()));	
+	}
+	
+	public function testConstructNoFile()
+	{
+		$this->setExpectedException('fValidationException');
+		$file = new fFile('output/fFile/five.txt');
+	}
+	
+	public function testConstructDirectory()
+	{
+		$this->setExpectedException('fValidationException');
+		$file = new fFile('output/fFile/');
+	}
+	
+	public function testToString()
+	{
+		$file = new fFile('output/fFile/one.txt');
+		$this->assertEquals('one.txt', $file->__toString());
+	}
+	
+	public function testGetName()
+	{
+		$file = new fFile('output/fFile/one.txt');
+		$this->assertEquals('one.txt', $file->getName());
+	}
+	
+	public function testGetParent()
+	{
+		$file = new fFile('output/fFile/one.txt');
+		$this->assertEquals(TRUE, $file->getParent() instanceof fDirectory);
+		$this->assertEquals(dirname($file->getPath()) . DIRECTORY_SEPARATOR, $file->getParent()->getPath());
+	}
+	
+	public function testGetPath()
+	{
+		$file = new fFile('output/fFile/one.txt');
+		$this->assertEquals('/output/fFile/one.txt', str_replace($_SERVER['DOCUMENT_ROOT'], '', $file->getPath()));
+	}
+	
+	public function testGetSize()
+	{
+		$file = new fFile('output/fFile/one.txt');
+		$this->assertEquals(3, $file->getSize());
+	}
+	
+	public function testRename()
+	{
+		$file = new fFile('output/fFile/one.txt');
+		$file->rename('three.txt', TRUE);
+		$this->assertEquals('three.txt', $file->getName());
+		$this->assertEquals($_SERVER['DOCUMENT_ROOT'] . '/output/fFile/', $file->getParent()->getPath());
+	}
+	
+	public function testRenameAnotherDirectory()
+	{
+		$file = new fFile('output/fFile/one.txt');
+		$file->rename('output/fFile2/two.txt', TRUE);
+		$this->assertEquals('two.txt', $file->getName());
+		$this->assertEquals($_SERVER['DOCUMENT_ROOT'] . '/output/fFile2/', $file->getParent()->getPath());
+	}
+	
+	public function testMove()
+	{
+		$file = new fFile('output/fFile/one.txt');
+		$file->move('output/fFile2/', TRUE);
+		$this->assertEquals('one.txt', $file->getName());
+		$this->assertEquals($_SERVER['DOCUMENT_ROOT'] . '/output/fFile2/', $file->getParent()->getPath());
+	}
+	
+	public function tearDown()
+	{
+		$dirs = array('output/fFile/', 'output/fFile2/');
+		foreach ($dirs as $dir) {
+			$files = array_diff(scandir($dir), array('.', '..'));
+			foreach ($files as $file) {
+				unlink($dir . $file);	
+			}
+			rmdir($dir);
+		}
+	}
+}
