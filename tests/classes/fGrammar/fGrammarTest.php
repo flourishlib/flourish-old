fa1bb9b5c31afd428b3e2f3226bfad2729ef9843
Will Bond <will@flourishlib.com>
2010-06-09 01:34:13 +0000

    Completed ticket #453 - fixed fGrammar::camelize() to work with human-friendly strings
    git-svn-id: http://svn.flourishlib.com/trunk@847 8e09ec71-114c-0410-a285-86ecef5c4c40

--- tests/classes/fGrammar/fGrammarTest.php
+++ tests/classes/fGrammar/fGrammarTest.php
@@ -18,6 +18,29 @@ class fGrammarTest extends PHPUnit_Framework_TestCase
 		}
 	}
 	
+	public static function camelizeProvider()
+	{
+		$output = array();
+		
+		$output[] = array('first_name', TRUE, 'FirstName');
+		$output[] = array('first_name', FALSE, 'firstName');
+		$output[] = array('first name', TRUE, 'FirstName');
+		$output[] = array('first name', FALSE, 'firstName');
+		$output[] = array('FIRST name', TRUE, 'FirstName');
+		$output[] = array('first NAME', FALSE, 'firstName');
+		$output[] = array('name', FALSE, 'name');
+		
+		return $output;
+	}
+	
+	/**
+	 * @dataProvider camelizeProvider
+	 */
+	public function testCamelize($input, $upper, $output)
+	{
+		$this->assertEquals($output, fGrammar::camelize($input, $upper));
+	}
+	
 	public function tearDown()
 	{
 		
