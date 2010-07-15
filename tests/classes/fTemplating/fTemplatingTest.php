68211e674dd035cced483e7537e60ea748252b11
Will Bond <will@flourishlib.com>
2010-07-15 05:53:45 +0000

    Added the methods fTemplating::delete() and fTemplating::remove()
    git-svn-id: http://svn.flourishlib.com/trunk@868 8e09ec71-114c-0410-a285-86ecef5c4c40

--- tests/classes/fTemplating/fTemplatingTest.php
+++ tests/classes/fTemplating/fTemplatingTest.php
@@ -75,6 +75,80 @@ class fTemplatingTest extends PHPUnit_Framework_TestCase
 		);
 	}
 	
+	public function testDelete()
+	{
+		$tmpl = new fTemplating();
+		$tmpl->set('foo', 1);
+		$tmpl->set('bar', 2);
+		$tmpl->delete('foo');
+		$this->assertEquals(
+			array(
+				'foo' => NULL,
+				'bar' => 2
+			),
+			$tmpl->get(array('foo', 'bar'))
+		);
+	}
+	
+	public function testRemove()
+	{
+		$tmpl = new fTemplating();
+		$tmpl->set('foo', array(1, 2, 3));
+		$tmpl->remove('foo', 1);
+		$this->assertEquals(
+			array(
+				2,
+				3
+			),
+			$tmpl->get('foo')
+		);
+	}
+	
+	public function testRemoveMultiple()
+	{
+		$tmpl = new fTemplating();
+		$tmpl->set('foo', array(1, 2, 1, 3, 1));
+		$tmpl->remove('foo', 1);
+		$this->assertEquals(
+			array(
+				2,
+				3
+			),
+			$tmpl->get('foo')
+		);
+	}
+	
+	public function testRemoveFuzzy()
+	{
+		$tmpl = new fTemplating();
+		$tmpl->set('foo', array(0, 1, 2, 3));
+		$tmpl->remove('foo', '');
+		$this->assertEquals(
+			array(
+				1,
+				2,
+				3
+			),
+			$tmpl->get('foo')
+		);
+	}
+	
+	public function testRemoveNonExistent()
+	{
+		$tmpl = new fTemplating();
+		$tmpl->set('foo', array(0, 1, 2, 3));
+		$tmpl->remove('bar', '');
+	}
+	
+	public function testRemoveNonArray()
+	{
+		$this->setExpectedException('fProgrammerException');
+		
+		$tmpl = new fTemplating();
+		$tmpl->set('foo', 1);
+		$tmpl->remove('foo', 1);
+	}
+	
 	public function tearDown()
 	{
 		
